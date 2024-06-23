26aee3e25964deb5af8d4507863de09e59b49916
Omar Massaoudi <162307601+MassaoudiOmar@users.noreply.github.com>
2024-06-23 15:19:05 +0000

    vfdvdfvdfvdfvdfv

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8000").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
